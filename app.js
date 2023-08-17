560db00c4467fca68ee9865aae53d3dd40312f99
Rophy Tsai <111145158+yltsaize@users.noreply.github.com>
2023-08-18 06:02:09 +0800

    feat: in-mem cache and /download API

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const axios = require('axios');
 const express = require('express');
 const { PubSub } = require('@google-cloud/pubsub');
 const { Storage } = require('@google-cloud/storage');
+const NodeCache = require( "node-cache" );
 
 const config = require('./config');
+
+const cache = new NodeCache( { stdTTL: config.retentionSeconds } );
 
 const app = express();
 
@@ -16,11 +19,47 @@ function startSubscription() {
   subscription.seek(seekDate);
 
   subscription.on('message', async (message) => {
-    console.log(message.data.toString());
+    try {
+      const { signed_url, webhook } = JSON.parse(message.data);
+      cache.set(signed_url, true);
+      console.log(signed_url, webhook);
+      } catch (err) {
+      console.error(err);
+    }
+    
   });
 }
 
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
   startSubscription();
+});
+
+app.get('/download', (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.status(400).send({
+    status: 'error',
+    message: 'missing required query "url"'
+  });
+
+  console.log(url);
+
+  return axios({
+    method: 'get',
+    url: url,
+    validateStatus: (status) => true,
+    responseType: 'stream',
+  })
+  .then(resp => {
+    res.status(resp.status);
+    resp.data.pipe(res);
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).send({
+      status: 'error',
+      message: 'unkonwn error'
+    });
+  });
+
 });
